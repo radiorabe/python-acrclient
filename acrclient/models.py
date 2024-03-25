67df3fc2a8fab872af1982af3e80068f01823da7
Lucas Bickel <116588+hairmare@users.noreply.github.com>
2024-03-25 21:15:05 +0100

    fix(types): use PEP-655 NotRequired (#191)

--- acrclient/models.py
+++ acrclient/models.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TypedDict
+from typing import NotRequired, TypedDict
 
 
 class GetBmCsProjectsResultsParams(TypedDict):  # pragma: no cover
@@ -12,9 +12,9 @@ class GetBmCsProjectsResultsParams(TypedDict):  # pragma: no cover
     """last: get the last results, day: get the day results, Default is day"""
     date: str
     """Get all the results on this date. The format is YYYYmmdd (E.g. 20210201)"""
-    min_duration: int | None
+    min_duration: NotRequired[int | None]
     """Return the results of played_duration >= min_duration seconds (default: 0)"""
-    max_duration: int | None
+    max_duration: NotRequired[int | None]
     """Return results with played_duration <= max_duration seconds (default: 3600)"""
-    isrc_country: str | None
+    isrc_country: NotRequired[str | None]
     """Only return results that match the isrc country code (E.g. DE, FR, IT, US)"""
